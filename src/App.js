85194099bd66d923b569a7cc9e6fe373be5d7bcf
Jean Mendonça <jean@handcom.com.br>
2021-01-27 16:13:36 -0300

    commit before redux config

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import Sidebar from './components/Sidebar'
 import Video from './components/Video'
 
 function App() {
+  let state = {
+    activeLesson: {},
+    modules: [
+      {
+        id: 1, title: 'Iniciando com React', lessons: [
+          { id: 1, title: 'Primeira aula' },
+          { id: 2, title: 'Segunda aula' }
+        ]
+      },
+      {
+        id: 2, title: 'Aprendendo REDUX', lessons: [
+          { id: 3, title: 'Terceira aula' },
+          { id: 4, title: 'Quarta aula' }
+        ]
+      }
+    ]
+  }
+
   return (
     <div className="App">
-      <Video />
-      <Sidebar />
+      <Video activeLesson={state.activeLesson} />
+      <Sidebar modules={state.modules} />
     </div>
   )
 }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,22 +1,5 @@
 import React from 'react'
 
-let state = {
-  modules: [
-    {
-      id: 1, title: 'Iniciando com React', lessons: [
-        { id: 1, title: 'Primeira aula' },
-        { id: 2, title: 'Segunda aula' }
-      ]
-    },
-    {
-      id: 2, title: 'Aprendendo REDUX', lessons: [
-        { id: 3, title: 'Terceira aula' },
-        { id: 4, title: 'Quarta aula' }
-      ]
-    }
-  ]
-}
-
 function Sidebar() {
   const { modules } = state
 
