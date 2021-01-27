a07f9e161fdb8c3213813817292afdcfc8658cb5
Jean Mendonça <jean@handcom.com.br>
2021-01-27 15:57:07 -0300

    basic structure before redux

---
+++ src/components/Video/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Video() {
+  return (
+    <div>
+      <strong>Módulo X</strong>
+      <br />
+      <span>Aula X</span>
+      <br /><br />
+    </div>
+  )
+}
+
+export default Video
