4179978c1ff6728345e09b4f4bd8b6d2b76ffab6
Jean Mendonça <jean@handcom.com.br>
2021-01-27 16:24:41 -0300

    redux working

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
+import React from 'react'
 import Sidebar from './components/Sidebar'
 import Video from './components/Video'
+import { Provider } from 'react-redux'
+import store from './store'
 
 function App() {
-  let state = {
-    activeLesson: {},
-    modules: [
-      {
-        id: 1, title: 'Iniciando com React', lessons: [
-          { id: 1, title: 'Primeira aula' },
-          { id: 2, title: 'Segunda aula' }
-        ]
-      },
-      {
-        id: 2, title: 'Aprendendo REDUX', lessons: [
-          { id: 3, title: 'Terceira aula' },
-          { id: 4, title: 'Quarta aula' }
-        ]
-      }
-    ]
-  }
-
   return (
     <div className="App">
-      <Video activeLesson={state.activeLesson} />
-      <Sidebar modules={state.modules} />
+      <Provider store={store}>
+        <Video />
+        <Sidebar />
+      </Provider>
     </div>
   )
 }
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,27 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-function Sidebar() {
-  const { modules } = state
+const Sidebar = ({ modules }) => (
+  <aside>
+    {
+      modules.map(module => (
+        <div key={module.id}>
+          <strong>{module.title}</strong>
 
-  return (
-    <aside>
-      {
-        modules.map(module => (
-          <div key={module.id}>
-            <strong>{module.title}</strong>
+          <ul>
+            {
+              module.lessons.map(lesson => (
+                <li key={lesson.id}>{lesson.title}</li>
+              ))
+            }
+          </ul>
+        </div>
+      ))
+    }
+  </aside>
+)
 
-            <ul>
-              {
-                module.lessons.map(lesson => (
-                  <li key={lesson.id}>{lesson.title}</li>
-                ))
-              }
-            </ul>
-          </div>
-        ))
-      }
-    </aside>
-  )
-}
-
-export default Sidebar
+export default connect(state => ({ modules: state }))(Sidebar)
